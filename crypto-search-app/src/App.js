5750a805038434b9541f8ce76a8c12fc6865860a
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2023-09-25 11:53:44 +0530

    slight changes

--- crypto-search-app/src/App.js
+++ crypto-search-app/src/App.js
@@ -9,14 +9,14 @@ function App() {
   const [coins, setCoins] = useState([]);
 
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en";
 
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
         setCoins(response.data);
-        console.log(response.data);
+        // console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
